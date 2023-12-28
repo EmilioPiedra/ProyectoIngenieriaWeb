984f922bc4ce8f7efd4eb1e1e06e87813fe66458
gilberthsolano <49965038+gilberthsolano@users.noreply.github.com>
2023-12-28 13:48:33 -0500

    cambio imagen portada

--- auth-front/src/rutas/Home.tsx
+++ auth-front/src/rutas/Home.tsx
@@ -1,9 +1,20 @@
 import DefaultLayout from '../layout/DefaultLayout';
-
+import imagen from '../imagenes/imagenportada.jpg';
 export default function Home() {
     return <>
         <DefaultLayout>
-            <h1>Home</h1>
+            <h1>ALQUILER DE BICICLETAS</h1>
+            <img src= {imagen}>
+            
+            </img>
+            
+            <div>
+                <h1></h1>
+            </div>
+
+
+
+
         </DefaultLayout>
     </>;
 }
