b2e228388cb4bbc030142a27cb42c996c26fef8d
gilberthsolano <49965038+gilberthsolano@users.noreply.github.com>
2023-12-28 16:29:32 -0500

    cambios

--- auth-front/src/rutas/Home.tsx
+++ auth-front/src/rutas/Home.tsx
@@ -1,18 +1,15 @@
 import DefaultLayout from '../layout/DefaultLayout';
 import imagen from '../imagenes/imagenportada.jpg';
+import '../App.css';
 export default function Home() {
     return <>
         <DefaultLayout>
-            <h1>ALQUILER DE BICICLETAS</h1>
-            <img src= {imagen}>
-            
-            </img>
-            
-            <div>
-                <h1></h1>
-            </div>
+        <div className="fondo-negro">
+        <h1 className="alquiler-title">ALQUILER DE BICICLETAS</h1>
+        
 
 
+        </div>
 
 
         </DefaultLayout>
