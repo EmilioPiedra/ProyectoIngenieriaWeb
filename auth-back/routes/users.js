c53b2b3dad2ac4b4a82fd70bf75f4159994e08ee
Emilio Piedra León <104231416+EmilioPiedra@users.noreply.github.com>
2024-01-03 23:45:00 -0500

    commit emilio14 el duro

--- auth-back/routes/users.js
+++ auth-back/routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { jsonResponse } = require('../lib/jsonResponse');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('users route');
+    res.status(200).json(jsonResponse(200, req.user));
 });
 
 module.exports = router;
--- auth-front/src/rutas/Dashboard.tsx
+++ auth-front/src/rutas/Dashboard.tsx
@@ -1,9 +1,12 @@
+import { useAuth } from '../auth/AuthProvider';
 import DefaultLayout from '../layout/DefaultLayout';
 
 export default function Dashboard() {
-    return <>
+    const auth = useAuth();
+
+    return (
         <DefaultLayout>
-            <h1>Dashboard</h1>
+            <h1>Dashboard de {auth.getUser()?.name}</h1>
         </DefaultLayout>
-    </>;;
+    );
 }
