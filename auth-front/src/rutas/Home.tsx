f0dd960d59cafdfc00507f4ad6f03127c6ed7091
gilberthsolano <49965038+gilberthsolano@users.noreply.github.com>
2024-01-17 22:10:12 -0500

    cambios gilbert

--- auth-front/src/rutas/Home.tsx
+++ auth-front/src/rutas/Home.tsx
@@ -16,12 +16,23 @@ export default function Home() {
         <div className="row">
           <div className="col-md-6">
             {/* Texto a la izquierda */}
-            <p className='texto'>
+            <p className='ventaja'>
               BikeRenta te ofrece servicio de alquiler de bicicletas para montaña, ruta y/o ciclo paseo en la modalidad que prefieras para disfrutar del ciclismo sin preocupaciones.
+
+            </p>
+            <br></br>
+            <p className='ventaja'>
+            <h3>Ventajas de utilizar bicicleta:</h3>
+            <ol>
+  <li><strong>Salud Cardiovascular:</strong> Andar en bicicleta mejora la salud cardiovascular al fortalecer el corazón y mejorar la circulación sanguínea, reduciendo el riesgo de enfermedades cardíacas.</li><br></br>
+  <li><strong>Ejercicio de Bajo Impacto:</strong> Es una forma de ejercicio de bajo impacto que disminuye la presión sobre las articulaciones, siendo amigable para personas de todas las edades y niveles de condición física.</li><br></br>
+  <li><strong>Sostenibilidad Ambiental:</strong> Contribuye a la sostenibilidad ambiental al ser una forma de transporte libre de emisiones, reduciendo la contaminación del aire y disminuyendo la huella de carbono.</li><br></br>
+  <li><strong>Bienestar Mental:</strong> Andar en bicicleta libera endorfinas, mejorando el estado de ánimo y reduciendo el estrés, la ansiedad y la depresión.</li><br></br>
+</ol>
+              
             </p>
           </div>
           <div className="col-md-6">
-            {/* Tabla a la derecha */}
             <table className="tabla mb-4">
               <thead>
                 <tr>
@@ -67,13 +78,13 @@ export default function Home() {
           <div className="col-md-6">
             <div className="requisitos">
               <h3>Requisitos:</h3>
-              <ul>
+              <ol>
                 <li>-Ser mayor de edad</li>
                 <li>-Recepción de datos personales y firma</li>
                 <li>-Documento cédula y licencia de conducir *Entregar en la tienda</li>
                 <li>-Para uso entrega de garantía obligatoria reembolsable</li>
                 <li>-Contrato (Leer)</li>
-              </ul>
+              </ol>
             </div>
           </div>
           <div className="col-md-6">
